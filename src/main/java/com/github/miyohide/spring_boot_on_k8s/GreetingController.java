0b49b64f1c038d63bae60d9a50bd78f77b85c4fa
Hidenori Miyoshi <miyohide@gmail.com>
2022-10-16 10:12:50 +0900

    create sample apps

---
+++ src/main/java/com/github/miyohide/spring_boot_on_k8s/GreetingController.java
@@ -0,0 +1,18 @@
+package com.github.miyohide.spring_boot_on_k8s;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
